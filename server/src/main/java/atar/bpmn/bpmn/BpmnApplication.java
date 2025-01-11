d138c5accae8090c72904127359b633d55c8c1cd
Paweł Hajdo <pawel.hajdo001@gmail.com>
2025-01-11 19:02:06 +0100

    initial commit

---
+++ server/src/main/java/atar/bpmn/bpmn/BpmnApplication.java
@@ -0,0 +1,13 @@
+package atar.bpmn.bpmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpmnApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/atar/bpmn/bpmn/BpmnApplicationTests.java
@@ -0,0 +1,13 @@
+package atar.bpmn.bpmn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
