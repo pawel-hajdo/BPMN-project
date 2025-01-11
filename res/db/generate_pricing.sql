529084279b8def35f4cdff553e14b4b7decfc2ef
GAL_son <gmail.com>
2025-01-11 22:19:19 +0100

    db start

---
+++ res/db/generate_pricing.sql
@@ -0,0 +1,6 @@
+INSERT INTO "schema.price_rate" ("id", "cost_per_standard_hour", "min_hours")
+VALUES
+    (DEFAULT, 500, 1),
+    (DEFAULT, 375, 2),
+    (DEFAULT, 300, 5),
+    (DEFAULT, 250, 10);
