bf4d734e1c98bf9b799e40cc314982de448c34fa
GAL_son <pancerek1@gmail.com>
2025-01-13 03:31:35 +0100

    all workers done

--- server/src/main/java/atar/bpmn/parking_reservations/worker/ReservationWorker.java
+++ server/src/main/java/atar/bpmn/parking_reservations/worker/ReservationWorker.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import atar.bpmn.parking_reservations.service.EmailService;
 import atar.bpmn.parking_reservations.service.EmitterService;
 import atar.bpmn.parking_reservations.service.ReservationService;
 
@@ -22,6 +23,7 @@ public class ReservationWorker {
 
     private final ReservationService reservationService;
     private final EmitterService emitterService;
+    private final EmailService emailService;
 
     @JobWorker(type = "check_for_spot")
     public Map<String, Object> checkForSpot(final JobClient client, final ActivatedJob job) {
@@ -122,8 +124,13 @@ public class ReservationWorker {
 
         System.out.println("send_ticket");
         JSONObject eventMessage = new JSONObject();
-        eventMessage.put("success", jobResultVariables.get("access_code").toString());
+        String accessCode = jobResultVariables.get("accessCode").toString();
+        String email = jobResultVariables.get("mail").toString();
+
+        eventMessage.put("success", accessCode);
         emitterService.sendMessageToListener(String.valueOf(job.getProcessInstanceKey()), eventMessage);
+
+        emailService.sendReservationSuccessEmail(email, accessCode);
 
         return jobResultVariables;
     }
